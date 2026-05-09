43591e70619967e842ee8c2a629a8078ff58d684
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-05-09 17:26:24 +0900

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apprun3-package control-app.apprunxproj -o CapturedQRAgent Controller.apprunx
+apprun3-package control-app.apprunxproj -o CapturedQRAgent-Controller.apprunx
 apprun3-package daemon.apprunxproj -o capturedqragentd.apprunx
 
